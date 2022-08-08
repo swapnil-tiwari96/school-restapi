d2a310182498479573afb3be11539d93af3959a1
Swapnil Tiwari <swaptiwari@outlook.com>
2022-08-08 18:59:01 +0530

    student controller models routes updated

--- controller/students.js
+++ controller/students.js
@@ -1,1 +1,36 @@
-const User = require("../models/user");
+const Student = require("../models/student");
+
+exports.getAll = (req, res) =>
+{
+    Student.find()
+        .exec()
+        .then(docs => res.status(200).send(docs)
+        )
+        .catch(err => res.status(500).send(err))
+}
+
+exports.addStudent = (req, res) =>
+{
+    const student = new Student({
+        name: req.body.name,
+        dob: req.body.dob
+    });
+    student.save()
+        .then(result => res.status(200).send(result))
+        .catch(error => res.status(500).send(error));
+}
+
+exports.getOne = (req, res) =>
+{
+    const id = req.params.orderID;
+    res.status(200).send(id);
+}
+
+exports.deleteOne = (req, res) =>
+{
+    const id = req.params.productID;
+    Student.deleteOne({ _id: id })
+        .exec()
+        .then(result => res.status(200).send(result))
+        .catch(err => err.status(500).send(err))
+}
--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const StudentController = require('../controller/students');
+const checkAuth = require('../middleware/checkAuth');
+const StudentsController = require('../controller/Students')
 
+router.get('/', StudentsController.getAll);
+
+router.post('/', checkAuth, StudentsController.addStudent);
+
+router.get('/:studentID', StudentsController.getOne);
+
+router.delete('/:studentID', checkAuth, StudentsController.deleteOne);
 
 module.exports = router;
